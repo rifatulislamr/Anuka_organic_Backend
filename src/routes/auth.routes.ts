6dc783c8842193cc7aa95fbf1e83531ae0321c1a
rifatulislamr <rifat.islam.cse@gmail.com>
2025-10-09 23:45:05 +0600

    feat: auth remove middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,15 +1,12 @@
 import { Router } from "express";
 import {
   changePasswordController,
-
   getUserByIdController,
-
   getUsersWithRoles,
   login,
   register,
   updateUserController,
 } from "../controllers/auth.controller";
-import { authenticateUser } from "../middlewares/auth.middleware";
 import { getUserList } from "../controllers/users.controller";
 
 const router = Router();
@@ -18,7 +15,7 @@ const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/users", authenticateUser, getUserList);
+router.get("/users", getUserList);
 router.get("/users-by-userId/:userId", getUserByIdController);
 router.get("/users-by-roles", getUsersWithRoles);
 router.put("/users/:userId", updateUserController);
