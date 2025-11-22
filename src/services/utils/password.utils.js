5869a65616152d16be11c283fbd7e19dc47bf2a5
rifatulislamr <rifat.islam.cse@gmail.com>
2025-11-22 21:19:55 +0600

    feat: passoword issue is updated

--- src/services/utils/password.utils.js
+++ src/services/utils/password.utils.js
@@ -46,25 +46,7 @@ const validatePassword = (password) => {
     if (password.length < MIN_LENGTH) {
         errors.push(`Password must be at least ${MIN_LENGTH} characters long`);
     }
-    if (password.length > MAX_LENGTH) {
-        errors.push(`Password must not exceed ${MAX_LENGTH} characters`);
-    }
-    if (!/[A-Z]/.test(password)) {
-        errors.push("Password must contain at least one uppercase letter");
-    }
-    if (!/[a-z]/.test(password)) {
-        errors.push("Password must contain at least one lowercase letter");
-    }
-    if (!/\d/.test(password)) {
-        errors.push("Password must contain at least one number");
-    }
-    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-        errors.push("Password must contain at least one special character");
-    }
-    const commonPasswords = ["password123", "admin123", "12345678"];
-    if (commonPasswords.includes(password.toLowerCase())) {
-        errors.push("Password is too common");
-    }
+    
     if (errors.length > 0) {
         throw (0, errors_utils_1.BadRequestError)(errors.join(". "));
     }
--- src/services/utils/password.utils.ts
+++ src/services/utils/password.utils.ts
@@ -20,7 +20,7 @@ export const hashPassword = async (password: string): Promise<string> => {
 
 export const comparePassword = async (
   password: string,
-  hash: string,
+  hash: string
 ): Promise<boolean> => {
   try {
     return await bcrypt.compare(password, hash);
@@ -38,27 +38,6 @@ export const validatePassword = (password: string): void => {
 
   if (password.length > MAX_LENGTH) {
     errors.push(`Password must not exceed ${MAX_LENGTH} characters`);
-  }
-
-  if (!/[A-Z]/.test(password)) {
-    errors.push("Password must contain at least one uppercase letter");
-  }
-
-  if (!/[a-z]/.test(password)) {
-    errors.push("Password must contain at least one lowercase letter");
-  }
-
-  if (!/\d/.test(password)) {
-    errors.push("Password must contain at least one number");
-  }
-
-  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-    errors.push("Password must contain at least one special character");
-  }
-
-  const commonPasswords = ["password123", "admin123", "12345678"];
-  if (commonPasswords.includes(password.toLowerCase())) {
-    errors.push("Password is too common");
   }
 
   if (errors.length > 0) {
